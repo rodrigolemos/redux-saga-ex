f03608005f3060ff85031440e47f5e9578260491
Rodrigo Lemos <rodrigolemosrl@hotmail.com>
2019-11-17 14:00:46 -0300

    Finishing Redux Saga

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -25,9 +25,9 @@ class Home extends Component {
     });
   }
 
-  handleAddProduct = product => {
-    const { addToCart } = this.props;
-    addToCart(product);
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
+    addToCartRequest(id);
   };
 
   render() {
@@ -47,7 +47,7 @@ class Home extends Component {
             <span>{product.priceFormatted}</span>
             <button
               type="button"
-              onClick={() => this.handleAddProduct(product)}
+              onClick={() => this.handleAddProduct(product.id)}
             >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF"/> {amount[product.id] || 0}
